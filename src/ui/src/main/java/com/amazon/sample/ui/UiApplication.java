f86dae6c5b41922c75c644d15ebd8adfbb297224
berni <bernizapico@gmail.com>
2026-06-10 22:46:22 -0300

    feat: finished adding something

--- src/ui/src/main/java/com/amazon/sample/ui/UiApplication.java
+++ src/ui/src/main/java/com/amazon/sample/ui/UiApplication.java
@@ -18,7 +18,6 @@
 
 package com.amazon.sample.ui;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
